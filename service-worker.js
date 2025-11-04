dc1a0c08aea9e223080ef0ae982741c3ca300d6c
George Akinian <Akinian@me.com>
2025-11-04 16:23:20 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const CACHE = 'df2025-v2';
+const ASSETS = ['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request).then(net => {
+      const copy = net.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return net;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
